8151d9e5c79fe6611c27efe858cb409bc0d4dbb5
kapesvishnu <104592314+kapesvishnu@users.noreply.github.com>
2025-08-20 20:26:22 -0500

    Add Flask server to properly serve static HTML files on Render

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, send_from_directory, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/src/advanced_chit_calculator.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
